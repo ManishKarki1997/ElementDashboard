9802a7bb82b7055a8a0bf6d0f7bf165dc7625ed9
ManishKarki1997 <manishkarki247@gmail.com>
2022-03-02 20:05:34 +0545

    project initialization and install element ui

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+import { Button } from "element-ui";
+
+Vue.config.productionTip = false;
+
+Vue.component(Button.name, Button);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
